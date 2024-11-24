369b6f6893e70215c84c9b5bd418bf50676f78a0
Daniël de Jonge <prive@danieldj.nl>
2024-11-25 00:29:15 +0100

    added the extra rows, basic working function

--- microservice/code/routes/index.js
+++ microservice/code/routes/index.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { responseExample, updateExample, responseByIdExample } from '../controllers/exampleController.js';
+import { responseExample, updateExample, responseByIdExample, responseMilestones, updateMilestone, responseShops, updateShop, responseStatistics, updateStatistic, responseTimers, updateTimer, responseUsers, updateUser, responseUserPreferences, updateUserPreference } from '../controllers/exampleController.js';
 import { checkName } from '../middleware/exampleMiddleware.js';
 const router = express.Router();
 
@@ -10,5 +10,23 @@ router.get('/', (req, res, next) => {
 router.get('/example', checkName, responseExample);
 router.post('/example', checkName, updateExample);
 router.get('/example/:id', checkName, responseByIdExample);
+
+router.get('/milestones', responseMilestones);
+router.post('/milestones', updateMilestone);
+
+router.get('/shops', responseShops);
+router.post('/shops', updateShop);
+
+router.get('/statistics', responseStatistics);
+router.post('/statistics', updateStatistic);
+
+router.get('/timers', responseTimers);
+router.post('/timers', updateTimer);
+
+router.get('/users', responseUsers);
+router.post('/users', updateUser);
+
+router.get('/user-preferences', responseUserPreferences);
+router.post('/user-preferences', updateUserPreference);
 
 export default router;
